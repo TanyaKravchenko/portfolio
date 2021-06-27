8aa7d6ca28072370023d000af83ba0a5a581c7de
Tanya Kravchenko <mail-tanja@mail.ru>
2021-06-27 23:55:46 +0300

    Refactored skills.

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -9,9 +9,12 @@ function Skills() {
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <h2 className={style.title}>Skills</h2>
                 <div className={style.skills}>
-                    <Skill title={'JS'} description={'Alias at, corporis cupiditate dolor dolorem, ex excepturi hic itaque iure labore molestias pariatur quaerat quo ratione sequi sint suscipit temporibus voluptatum?'}/>
-                    <Skill title={'React'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at, corporis cupiditate dolor dolorem, ex excepturi hic itaque iure labore molestias pariatur quaerat quo ratione sequi sint suscipit temporibus voluptatum?'}/>
-                    <Skill title={'CSS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at, corporis cupiditate dolor dolorem, ex excepturi hic itaque iure labore molestias pariatur quaerat quo ratione sequi sint suscipit temporibus voluptatum?Alias at, corporis cupiditate dolor dolorem, ex excepturi hic itaque iure labore molestias pariatur quaerat quo ratione sequi sint suscipit temporibus voluptatum?'}/>
+                    <Skill title={'JS'}
+                           description={'Alias at, corporis cupiditate dolor dolorem, ex excepturi hic itaque iure labore molestias pariatur quaerat quo ratione sequi sint suscipit temporibus voluptatum?'}/>
+                    <Skill title={'React'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at, corporis cupiditate dolor dolorem, ex excepturi hic itaque iure labore molestias pariatur quaerat quo ratione sequi sint suscipit temporibus voluptatum?'}/>
+                    <Skill title={'CSS'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at, corporis cupiditate dolor dolorem, ex excepturi hic itaque iure labore molestias pariatur quaerat quo ratione sequi sint suscipit temporibus voluptatum?Alias at, corporis cupiditate dolor dolorem, ex excepturi hic itaque iure labore molestias pariatur quaerat quo ratione sequi sint suscipit temporibus voluptatum?'}/>
                 </div>
             </div>
 
