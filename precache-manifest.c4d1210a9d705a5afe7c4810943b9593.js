d0cb5b970419a29f6082e25fd4b8f08fa4b18b7e
Tanya Kravchenko <mail-tanja@mail.ru>
2021-07-15 22:01:26 +0300

    Updates

---
+++ precache-manifest.c4d1210a9d705a5afe7c4810943b9593.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177b90f45a5283e277b46f8ee0648e3d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5e103417f6d906e1b438",
+    "url": "/portfolio/static/css/main.a6666a3f.chunk.css"
+  },
+  {
+    "revision": "8343cd71869a07d380d0",
+    "url": "/portfolio/static/js/2.b4b604e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b4b604e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e103417f6d906e1b438",
+    "url": "/portfolio/static/js/main.d4085b71.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "8af5d51cb158d3ecbc7ee4047a8cae28",
+    "url": "/portfolio/static/media/Grid-14-old-cunts.8af5d51c.jpg"
+  }
+]);
