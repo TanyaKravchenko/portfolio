af0c87cd11b7b5b5bee0e4e786aad76002c736a6
Tanya Kravchenko <mail-tanja@mail.ru>
2021-07-20 01:00:39 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2ab1bd6d1827b534e9e66af7b6137828.js"
+  "/portfolio/precache-manifest.a5623a1be2f82219ad09450c2ff8b00e.js"
 );
 
 self.addEventListener('message', (event) => {
